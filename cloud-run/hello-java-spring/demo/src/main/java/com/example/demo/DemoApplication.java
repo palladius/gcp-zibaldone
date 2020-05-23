7f5e9a951389deabe7016c90343531aa903e8fb8
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-05-23 14:34:00 +0200

    broken still

--- cloud-run/hello-java-spring/demo/src/main/java/com/example/demo/DemoApplication.java
+++ cloud-run/hello-java-spring/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 public class DemoApplication {
 
